cee0b401e9afcf5debf0e782cf5a898c7c4a5ce2
Johnathan Branch <johnathan.branch@ymail.com>
2021-10-11 05:23:18 -0700

    Added motivational phrase

--- greetings.py
+++ greetings.py
@@ -1,2 +1,9 @@
 print("Hello World!")
-print("Goodbye")
+print("Welcome My Friend!")
+
+# Show somes cool greetings image
+
+# Ask the user what he/she wants to drink
+
+# If you can dream it, you can do it! (probably, depends if its illegal or violates the laws of physics of course!)
+
